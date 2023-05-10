0bb2924d64761faefdeefc276c78ceda5d23c7d5
pranav <pranav.singh@incred.com>
2023-05-10 21:15:12 +0530

    added sorting, operator, group by

--- student_database.sql
+++ student_database.sql
@@ -15,7 +15,8 @@ create table student_personal_details (
 	gender VARCHAR(10),
 	weight INTEGER(10),
 	height INTEGER(10),
-	blood_group VARCHAR(10)
+	blood_group VARCHAR(10),
+    PRIMARY KEY(SPD_ID)
 );
 DESCRIBE student_personal_details;
 
@@ -26,7 +27,8 @@ create table student_education_details (
     class INTEGER,
 	batch VARCHAR(50),
 	department VARCHAR(100),
-	school VARCHAR(255)
+	school VARCHAR(255),
+    PRIMARY KEY(SED_ID)
 );
 DESCRIBE student_education_details;
 
@@ -58,16 +60,16 @@ DESCRIBE occupations;
 -- insert data into student_personal_details
 
 INSERT INTO student_personal_details (SPD_ID, name, date_of_birth, phone_number, email_id, gender, weight, height, blood_group) 
-VALUES (1, "RAMESH", "25/07/1994", "766853452", "ramesh@gmail.com", "M", "75", "182", "A+")
+VALUES (1, "RAMESH", "25/07/1994", 766853452, "ramesh@gmail.com", "M", 75, 182, "A+");
 
 INSERT INTO student_personal_details (SPD_ID, name, date_of_birth, phone_number, email_id, gender, weight, height, blood_group) 
-VALUES (2, "MAHESH", "10/05/1990", "945853452", "mahesh@gmail.com", "M", "65", "176", "0+")
+VALUES (2, "MAHESH", "10/05/1990", 945853452, "mahesh@gmail.com", "M", 65, 176, "0+");
 
 INSERT INTO student_personal_details (SPD_ID, name, date_of_birth, phone_number, email_id, gender, weight, height, blood_group) 
-VALUES (3, "KAMLESH", "12/04/1997", "855853452", "kamlesh@gmail.com", "M", "78", "165", "0-")
+VALUES (3, "KAMLESH", "12/04/1997", 855853452, "kamlesh@gmail.com", "M", 78, 165, "0-");
 
 INSERT INTO student_personal_details (SPD_ID, name, date_of_birth, phone_number, email_id, gender, weight, height, blood_group) 
-VALUES (4, "RIYA", "11/03/1997", "843853452", "riya@gmail.com", "F", "15", "155", "0+")
+VALUES (4, "RIYA", "11/03/1997", 843853452, "riya@gmail.com", "F", 15, 155, "0+");
 
 -- insert data into student_education_details
 INSERT INTO students_class_details (SED_ID, roll, class, batch, department,school)
@@ -80,6 +82,43 @@ VALUES (3, 7345, 4, 'a', 'Biology', 'DPS');
 INSERT INTO student_family_details (SFD_ID, father_name, mother_name, address, no_of_siblings, uncle_name, aunt_name, father_occupation, mother_occupation, no_of_family_member) 
 VALUES (1, "ajeet maheshwari", "kalpana maheshwari", "orissa", 3, "rakesh maheshwari", "sumitra maheshwari", 1, 3, 8);
 
--- view all the data inside a table
+-- view/filter the data inside a table
 SELECT * FROM student_personal_details
+select name, phone_number from student_personal_details where weight > 60;
+select name, phone_number from student_personal_details where weight < 60;
+select name, phone_number from student_personal_details where weight between 60 and 77;
+select MAX(weight) from student_personal_details;
+select MIN(weight) from student_personal_details;
+
+
+-- alter table sytax and working:
+alter table student_personal_details add age INT;
+alter table student_personal_details drop column blood_group;
+alter table student_personal_details modify column blood_group varchar(255);
+
+ALTER TABLE student_personal_details ADD PRIMARY KEY (SPD_ID);
+
+
+-- ********** very important(diffrence between drop and truncate command)
+-- it will drop the entire table, including the stucture
+DROP TABLE student_personal_details;
+
+-- delete the entire data in the table, without deleting the stucture
+TRUNCATE student_personal_details
+
+
+-- sorting the data in ascending and descending:
+select * from student_personal_details order by height ASC;
+select * from student_personal_details order by height DESC;
+select * from student_personal_details order by weight ASC;
+
+-- limit and offset
+select * from student_personal_details limit 2;
+select * from student_personal_details limit 3, 2;
+
+-- group by tutorail
+select sum(weight) from student_personal_details group by gender order by gender DESC;
+select sum(weight) from student_personal_details group by gender;
+select sum(weight) from student_personal_details group by gender order by gender ASC;
+select avg(weight) from student_personal_details group by gender order by gender ASC;
 
