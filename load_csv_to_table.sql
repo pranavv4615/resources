18c5d9aa39477b5bf8450c37d954dde1f1233418
pranav <pranav.singh@incred.com>
2023-05-13 20:10:11 +0530

    added employee database

---
+++ load_csv_to_table.sql
@@ -0,0 +1,4 @@
+LOAD DATA INFILE '/Users/incred/Documents/teaching_resources/Employee-Database-SQL-mystery/Employee-SQL/data/departments.csv' INTO TABLE departments FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+
+LOAD DATA INFILE '/Users/incred/Documents/teaching_resources/Employee-Database-SQL-mystery/Employee-SQL/data/departments.csv' INTO TABLE departments FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
---
+++ table_schemata.sql
@@ -0,0 +1,83 @@
+DROP TABLE
+  IF EXISTS departments;
+
+
+CREATE TABLE
+  departments (
+    dept_no VARCHAR(4) NOT NULL,
+    dept_name VARCHAR(40) NOT NULL,
+    UNIQUE (dept_name),
+    PRIMARY KEY (dept_no)
+  );
+
+
+DROP TABLE
+  IF EXISTS employees;
+
+
+CREATE TABLE
+  employees (
+    emp_no INT NOT NULL,
+    emp_title VARCHAR NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    gender VARCHAR NOT NULL,
+    hire_date DATE NOT NULL,
+    UNIQUE (emp_title),
+    PRIMARY KEY (emp_no)
+  );
+
+
+DROP TABLE
+  IF EXISTS dept_emp;
+
+
+CREATE TABLE
+  dept_emp (
+    emp_no INT NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
+    PRIMARY KEY (emp_no, dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+  );
+
+
+DROP TABLE
+  IF EXISTS dept_manager;
+
+
+CREATE TABLE
+  dept_manager (
+    dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    PRIMARY KEY (emp_no, dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+  );
+
+
+DROP TABLE
+  IF EXISTS salaries;
+
+
+CREATE TABLE
+  salaries (
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
+    PRIMARY KEY (emp_no),
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+  );
+
+
+DROP TABLE
+  IF EXISTS titles;
+
+
+CREATE TABLE
+  titles (
+    title_id VARCHAR NOT NULL,
+    title VARCHAR NOT NULL,
+    PRIMARY KEY (title_id),
+    FOREIGN KEY (title_id) REFERENCES employees (emp_title)
+  );
