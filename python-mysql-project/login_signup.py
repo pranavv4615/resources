dc2c70c72f3e4191f0bdddde05976cfa1bd9fcdc
pranav <pranav.singh@incred.com>
2023-05-18 20:58:33 +0530

    added home assignment

--- python-mysql-project/login_signup.py
+++ python-mysql-project/login_signup.py
@@ -18,10 +18,12 @@ result = cursor.fetchall()
 print("result: ", result)
 
 if len(result) == 0:
+    #user doesnot exit in the database table
     # Insert this data into the table (users)
     cursor.execute(f"INSERT INTO users (user_id, username, password) VALUES ({user_id}, '{username}', '{password}')")
     connection.commit()
 else:
+    #User exist in database table
     # Case 1: Password matches
     if result[0][2] == password:
         print("Login Successful")
@@ -29,4 +31,14 @@ else:
         print("Login Failed")
 
     
+
+#create a table students
+#student will be having field as student_id(int, primary_key), student_name varchar, student_school_name varchar, student_father_name varchar, student_age
+#filters:
+#const student_name = "pranav";
+# cursor.execute(f"SELECT * FROM students WHERE student_name = '{student_name}'")
+#const student_age = 18
+# cursor.execute(f"SELECT * FROM students WHERE student_age = '{student_age}'")
+#const student_school_name = "abc school"
+# cursor.execute(f"SELECT * FROM students WHERE student_school_name = '{student_school_name}'")
 
