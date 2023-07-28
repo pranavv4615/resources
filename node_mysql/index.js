2318f9065cc2684bde51da91014a4423dfe9dae2
pranav <pranav.singh@incred.com>
2023-07-28 21:53:38 +0530

    added mongo db code

--- node_mysql/index.js
+++ node_mysql/index.js
@@ -3,11 +3,12 @@ const app = express()
 const mysql = require('mysql2');
 
 const cors = require('cors')
-
-
 const port = 8001
 
+const { MongoClient } = require('mongodb');
+const uri = "";
 
+const client = new MongoClient(uri);
 
 
 app.get('/', (req, res) => {
@@ -20,8 +21,26 @@ app.get("/employee/details", cors(), async function(req, res){
     const connection = await mysql.createConnection({host:'127.0.0.1', user: 'root', password:'', database: 'employee_db'});
     const [rows, fields] = await connection.execute('SELECT * FROM employee');
 
-      res.status(200).json({status: true, response:rows })
-    
+      res.status(200).json({status: true, response:rows })  
+})
+
+app.get("/restraunt/details", cors(), async function(req, res){
+
+  await client.connect()
+  const database =  client.db("restraunt_db");
+  const collection =  database.collection("restaurant")
+
+
+  const cursor = await collection.find({"name": "pranav restraunt"})
+
+  
+  const data = []
+  await cursor.forEach(details => {
+    console.log(details)
+    data.push(details)
+  });
+
+  res.status(200).json({status: true, response:data }) 
 })
 
 
